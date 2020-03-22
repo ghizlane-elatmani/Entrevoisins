da438c5427d31b2b55f64cf1e241d41491730d36
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2020-03-22 19:04:39 +0100

    1.1.6 - Toolbar

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
@@ -1,14 +1,19 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
 import com.openclassrooms.entrevoisins.R;
@@ -40,6 +45,9 @@ public class DetailNeighbourActivity extends AppCompatActivity {
     @BindView(R.id.aboutMeTextView)
     TextView aboutMeTextView;
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolBar;
+
     Neighbour neighbour;
     boolean isFavorite;
 
@@ -49,6 +57,7 @@ public class DetailNeighbourActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_neighbour);
         ButterKnife.bind(this);
 
+        //Intent
         Intent intent = getIntent();
         neighbour = intent.getParcelableExtra("neighbour");
 
@@ -64,7 +73,7 @@ public class DetailNeighbourActivity extends AppCompatActivity {
                 .load(avatarUrl)
                 .into(avatarUrlImageView);
 
-
+        //FavoriteButton
         favoriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,11 +81,29 @@ public class DetailNeighbourActivity extends AppCompatActivity {
 
                 if(isFavorite == true) {
                     favoriteButton.setImageResource(R.drawable.ic_star_yellow_24dp);
+                    //intent
                 } else {
                     favoriteButton.setImageResource(R.drawable.ic_star_border_black_24dp);
                 }
             }
         });
+
+        //ToolBar
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
